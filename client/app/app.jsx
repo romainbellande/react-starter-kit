f248d4e87282b1a966978c926e450570a8d412b1
Romain Bellande <rbellande@keley-live.com>
2017-10-02 17:15:52 +0200

    init

---
+++ client/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <div  className="title">
+      <h1>Hello World</h1>
+    </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+				test: /\.scss?$/,
+				loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+			}
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin('style.css')]
+}
